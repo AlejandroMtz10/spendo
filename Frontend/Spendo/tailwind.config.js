e809007a115345a2b9fcbeb5bf8409c04f04bec7
Alejandro Martínez Rivera <alex10mtz_rivera@outlook.com>
2026-02-24 16:44:55 -0600

    Feat:  Setup frontend project
    Feat:  Setup frontend project with react js with vite, Tailwind css & react router dom

---
+++ Frontend/Spendo/tailwind.config.js
@@ -0,0 +1,14 @@
+ {import('tailwindcss').Config}
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./src/Pages/**/*.{js,ts,jsx,tsx}",
+        "./src/Components/**/*.{js,ts,jsx,tsx}"
+    ],
+    darkMode: "class",
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
