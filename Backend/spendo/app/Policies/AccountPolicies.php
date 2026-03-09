b788b73fc253fc02b5ee722509f2603289da2a58
Alejandro Martínez Rivera <alex10mtz_rivera@outlook.com>
2026-03-09 16:43:15 -0500

    Feat: Add Policies to verify the user data
    Feat: Add Policies to verify the user data on their accounts

---
+++ Backend/spendo/app/Policies/AccountPolicies.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Account;
+use App\Models\User;
+
+class AccountPolicy
+{
+    // Just the owner can view it
+    public function view(User $user, Account $account): bool
+    {
+        return $user->user_id === $account->user_id;
+    }
+
+    // Just the owner can update it
+    public function update(User $user, Account $account): bool
+    {
+        return $user->user_id === $account->user_id;
+    }
+
+    // Just the owner can delete it
+    public function delete(User $user, Account $account): bool
+    {
+        return $user->user_id === $account->user_id;
+    }
+}
